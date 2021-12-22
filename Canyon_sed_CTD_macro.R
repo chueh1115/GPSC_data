7d5f474d50f14d0e8f3d0345d59a0ad93d2aa6a8
chueh1115 <sarah861016@gmail.com>
2021-12-22 21:26:19 +0800

    fix

--- Canyon_sed_CTD_macro.R
+++ Canyon_sed_CTD_macro.R
@@ -36,149 +36,36 @@ levels(factor(GPSC_macro_abundance$Station))
 Canyon_macro_abundance<-GPSC_macro_abundance%>% filter(Station %in% c("GC1","GC2","GC3","GC4","GC5","GC6",
                                                                 "SC1","SC2","SC3",
                                                                 "FC1", "FC2", "FC2A", "FC3", "FC4", "FC5A"))
- #taxa
-#Polychaeta
-Polychaeta <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                         sheet = "Polychaeta",col_types = c("text","text", "text", 
-                                                            "numeric", "numeric", 
-                                                            "text", "text", "numeric",
-                                                            "numeric","text", "text",
-                                                            "numeric", "numeric",
-                                                            "numeric", "numeric", "numeric",
-                                                            "numeric", "text"))
-#Amphipoda
-Amphipoda <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                         sheet = "Amphipoda",col_types = c("text","text", "text", 
-                                                           "numeric", "numeric", 
-                                                           "text", "text", "numeric",
-                                                           "numeric","text", "text",
-                                                           "numeric", "numeric",
-                                                           "numeric", "numeric", "numeric",
-                                                           "numeric", "text"))
-#Isopoda
-Isopoda <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                        sheet = "Isopoda",col_types = c("text","text", "text", 
-                                                        "numeric", "numeric", 
-                                                        "text", "text", "numeric",
-                                                        "numeric","text", "text",
-                                                        "numeric", "numeric",
-                                                        "numeric", "numeric", "numeric",
-                                                        "numeric", "text"))
-#Tanaidacea
-Tanaidacea <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Tanaidacea",col_types = c("text","text", "text", 
-                                                         "numeric", "numeric", 
-                                                         "text", "text", "numeric",
-                                                         "numeric","text", "text",
-                                                         "numeric", "numeric",
-                                                         "numeric", "numeric", "numeric",
-                                                         "numeric", "text"))
-#Cumacea
-Cumacea <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Cumacea",col_types = c("text","text", "text", 
-                                                      "numeric", "numeric", 
-                                                      "text", "text", "numeric",
-                                                      "numeric","text", "text",
-                                                      "numeric", "numeric",
-                                                      "numeric", "numeric", "numeric",
-                                                      "numeric", "text"))
 
-#Osrtracoda
-Osrtracoda <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Osrtracoda",col_types = c("text","text", "text", 
-                                                         "numeric", "numeric", 
-                                                         "text", "text", "numeric",
-                                                         "numeric","text", "text",
-                                                         "numeric", "numeric",
-                                                         "numeric", "numeric", "numeric",
-                                                         "numeric", "text"))
-#Scaphopoda
-Scaphopoda <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Scaphopoda",col_types = c("text","text", "text", 
-                                                         "numeric", "numeric", 
-                                                         "text", "text", "numeric",
-                                                         "numeric","text", "text",
-                                                         "numeric", "numeric",
-                                                         "numeric", "numeric", "numeric",
-                                                         "numeric", "text"))
-#Aplacophora
-Aplacophora <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Aplacophora",col_types = c("text","text", "text", 
-                                                          "numeric", "numeric", 
-                                                          "text", "text", "numeric",
-                                                          "numeric","text", "text",
-                                                          "numeric", "numeric",
-                                                          "numeric", "numeric", "numeric",
-                                                          "numeric", "text"))
-#Sipuncula
-Sipuncula <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Sipuncula",col_types = c("text","text", "text", 
-                                                        "numeric", "numeric", 
-                                                        "text", "text", "numeric",
-                                                        "numeric","text", "text",
-                                                        "numeric", "numeric",
-                                                        "numeric", "numeric", "numeric",
-                                                        "numeric", "text"))
-#Harpacticoida
-Harpacticoida <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Harpacticoida",col_types = c("text","text", "text", 
-                                                            "numeric", "numeric", 
-                                                            "text", "text", "numeric",
-                                                            "numeric","text", "text",
-                                                            "numeric", "numeric",
-                                                            "numeric", "numeric", "numeric",
-                                                            "numeric", "text"))
-#Echinoderm
-Echinoderm <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Echinoderm",col_types = c("text","text", "text", 
-                                                         "numeric", "numeric", 
-                                                         "text", "text", "numeric",
-                                                         "numeric","text", "text",
-                                                         "numeric", "numeric",
-                                                         "numeric", "numeric", "numeric",
-                                                         "numeric", "text"))
-#Oligochaeta
-Oligochaeta <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Oligochaeta",col_types = c("text","text", "text", 
-                                                          "numeric", "numeric", 
-                                                          "text", "text", "numeric",
-                                                          "numeric","text", "text",
-                                                          "numeric", "numeric",
-                                                          "numeric", "numeric", "numeric",
-                                                          "numeric", "text"))
-#Bivalvia
-Bivalvia <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Bivalvia", col_types = c("text","text", "text", 
-                                                        "numeric", "numeric", 
-                                                        "text", "text", "numeric",
-                                                        "numeric","text", "text",
-                                                        "numeric", "numeric",
-                                                        "numeric", "numeric", "numeric",
-                                                        "numeric", "text"))
-#Nemertea
-Nemertea <- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
-                      sheet = "Nemertea",col_types = c("text","text", "text", 
-                                                       "numeric", "numeric", 
-                                                       "text", "text", "numeric",
-                                                       "numeric","text", "text",
-                                                       "numeric", "numeric",
-                                                       "numeric", "numeric", "numeric",
-                                                       "numeric", "text"))
-GPSC_macro_size<-rbind(Polychaeta,Amphipoda,Isopoda,Tanaidacea,Cumacea,
-                       Osrtracoda,Scaphopoda,Aplacophora,Sipuncula,
-                       Harpacticoida,Echinoderm,Oligochaeta,Bivalvia,Nemertea)
+GPSC_macro_size<-NULL
+for (i in 1:15){
+  df<- read_excel("data/GPSC_macro_size/GPSC_macro_size_2020.08.03.xlsx",
+                  sheet = i ,col_types = c("text","text", "text", 
+                                                     "numeric", "numeric", 
+                                                     "text", "text", "numeric",
+                                                     "numeric","text", "text",
+                                                     "numeric", "numeric",
+                                                     "numeric", "numeric", "numeric",
+                                                     "numeric", "text"))
+  GPSC_macro_size<-rbind(GPSC_macro_size,df)
+}
+
 levels(factor(GPSC_macro_size$Station))
 Canyon_macro_size<-GPSC_macro_size%>% filter(Station %in% c("GC1","GC2","GC3","GC4","GC5","GC6",
                                                             "SC1","SC2","SC3",
                                                             "FC1", "FC2", "FC2A", "FC3", "FC4", "FC5A"))
 
-macro <- createWorkbook() # workbook=excel in R
-addWorksheet(macro, "cruise") # create sheet1
-addWorksheet(macro, "abundance")#create sheet2
-addWorksheet(macro, "size")#create sheet3
-writeData(macro, "cruise", Canyon_macro_cruise) 
-writeData(macro, "abundance", Canyon_macro_abundance)
-writeData(macro, "size", Canyon_macro_size)
-saveWorkbook(macro, "Canyon_macro.xlsx", overwrite = TRUE)
+# macro <- createWorkbook() # workbook=excel in R
+# addWorksheet(macro, "cruise") # create sheet1
+# addWorksheet(macro, "abundance")#create sheet2
+# addWorksheet(macro, "size")#create sheet3
+# writeData(macro, "cruise", Canyon_macro_cruise) 
+# writeData(macro, "abundance", Canyon_macro_abundance)
+# writeData(macro, "size", Canyon_macro_size)
+# saveWorkbook(macro, "Canyon_macro.xlsx", overwrite = TRUE)
 
-
+# writexl example
+write_xlsx(list(Cruise = Canyon_macro_cruise,
+                Abundance = Canyon_macro_abundance,
+                Size = Canyon_macro_size),
+           "Canyon_macro.xlsx")
